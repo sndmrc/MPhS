1bf49bbd6952ce24614b4df409b2fc7f1c5500ae
gcemodels <gcemodels@gmail.com>
2024-11-24 20:01:35 +0100

    RPKMdataV3 added

---
+++ R/RPKMdataV3.R
@@ -0,0 +1,20 @@
+#' A dataset with V3 gene annotation containing RPKM expression data.
+#'
+#' @author G.B. Tornielli, M. Sandri, M. Fasoli, A. Amato, M. Pezzotti, P. Zuccolotto and S. Zenoni (\email{sandri.marco@gmail.com})
+#' @description In this file, there are all the objects required for projecting a new dataset onto the MPhS.
+#' @references Unpublished
+#' @usage data(RPKMdataV3)
+#' @format 
+#' A data frame with 43664 rows and 7 columns.
+#' \describe{
+#'  \item{gene_id}{A unique identifier for each gene (Vitvi annotation).}
+#'  \item{CT0R1}{Sample #1, Cultivar=Corvina, Stage=T0, Replicate=1}
+#'  \item{CT0R2}{Sample #2, Cultivar=Corvina, Stage=T0, Replicate=2}
+#'  \item{CT0R3}{Sample #3, Cultivar=Corvina, Stage=T0, Replicate=3}
+#'  \item{CT1R1}{Sample #4, Cultivar=Corvina, Stage=T1, Replicate=1}
+#'  \item{CT1R2}{Sample #5, Cultivar=Corvina, Stage=T1, Replicate=2}
+#'  \item{CT1R3}{Sample #6, Cultivar=Corvina, Stage=T1, Replicate=3}
+#' }
+#' @examples
+#' data(RPKMdataV3)
+"RPKMdataV3"
